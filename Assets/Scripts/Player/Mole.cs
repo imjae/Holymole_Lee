b5d77013f6a7a833bb230e38b5c68491929c427a
imjae <code@kakao.com>
2021-11-24 13:36:51 +0900

    캐릭터 이동, 중력 적용

--- Assets/Scripts/Player/Mole.cs
+++ Assets/Scripts/Player/Mole.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Mole : Player
 {
     public float moveSpeed = 10f;
-    public bool isMovement = true;
     public float groundDistance = 2f;
     public Transform groundCheckPosition;
     public LayerMask groundLayer;
+    
+    public float jumpHeight = 0.1f;
+
+
+    public bool isMovement = true;
     public bool isGrounded = true;
+    public bool isAttacked = false;
 
 
-    float gravity = -9.81f;
+    float gravity = -19.62f;
     public CharacterController controller;
     Vector3 moveDirection;
+    public Vector3 velocity;
     Animator animator;
 
     // Start is called before the first frame update
@@ -31,17 +37,20 @@ public class Mole : Player
     {
         isGrounded = gameObject.GroundCheck(groundCheckPosition, groundLayer, groundDistance);
 
+        ApplyGravity();
+        
         InputMovement();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            InputJump();
     }
 
     void InputMovement()
     {
-
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        Vector3 moveDirection =
-                    forward * Input.GetAxisRaw("Vertical") +
+        moveDirection =  forward * Input.GetAxisRaw("Vertical") +
                     right * Input.GetAxisRaw("Horizontal");
 
         float percent;
@@ -62,5 +71,30 @@ public class Mole : Player
         animator.SetFloat("RunPercent", percent, 0.1f, Time.deltaTime);
 
         controller.Move(moveDirection.normalized * finalSpeed * Time.deltaTime);
+    }
+
+    void InputJump()
+    {
+        if (isAttacked)
+        {
+            // 공격중 : 대쉬
+            Debug.Log("대쉬!");
+        }
+        else
+        {
+            // 일반상태 : 점프
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+
+    void ApplyGravity()
+    {
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
