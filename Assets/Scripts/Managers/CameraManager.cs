1d9339d0649dffcb5f3b18f04f9b16ed16c82cd5
imjae <code@kakao.com>
2021-11-26 13:16:53 +0900

    카메라 이름변경,카메라메니저 기본 구조 설정

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,50 @@ public class CameraManager : MonoBehaviour
     // TODO Camera는 VR의 캐릭터가 될 예정
     // 카메라의 위치를 담고있는 리스트
     // 게임 실행 시점에서 Resources 폴더에 있는 카메라 위치 전부 읽어서 리스트에 저장
-    public LinkedList<Transform> CameraTransformList;
+    public LinkedList<Transform> cameraTransformList;
 
-    public Transform current;
+    public LinkedListNode<Transform> currentNode;
 
-    // Start is called before the first frame update
+    private Camera mainCamera;
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
+
+        Transform[] tmpArr = Resources.LoadAll<Transform>("CameraTransform");
+        cameraTransformList = new LinkedList<Transform>(tmpArr);
+
+        // 첫 번째 노드 초기화
+        currentNode = cameraTransformList.First;
+        mainCamera = Camera.main;
+
+        TransferCamera(currentNode);
     }
 
-    // Update is called once per frame
-    void Update()
+    // 노드의 위치값을 카메라에 덮어쓰기
+    private void TransferCamera(LinkedListNode<Transform> node)
     {
-        
+        mainCamera.transform.position = node.Value.position;
+        mainCamera.transform.rotation = node.Value.rotation;
+    }
+
+    private LinkedListNode<Transform> NextNode()
+    {
+        currentNode = currentNode.Next;
+        return currentNode;
+    }
+
+    private LinkedListNode<Transform> PreviousNode()
+    {
+        currentNode = currentNode.Previous;
+        return currentNode;
+    }
+
+    public void NextCamera()
+    {
+        TransferCamera(NextNode());
+    }
+    public void PreviousCamera()
+    {
+        TransferCamera(PreviousNode());
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(gameObject);
     }
 }
