981ecdedcb155d08140a6f8e75d4c52a0821543c
imjae <code@kakao.com>
2021-11-29 11:27:30 +0900

    장애물 판별해 캐릭터가 미는 모션 주는 로직 추가

---
+++ Assets/PushingCheckPoint.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingCheckPoint : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (transform.parent.TryGetComponent<Mole>(out Mole mole))
+        {
+            GameObject target = other.gameObject;
+            if (target.TryGetComponent<Obtacle>(out Obtacle t))
+            {
+                mole.animator.SetTrigger("Pushing");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (transform.parent.TryGetComponent<Mole>(out Mole mole))
+        {
+            mole.animator.Play("Run Blend Tree");
+        }
+    }
+}
