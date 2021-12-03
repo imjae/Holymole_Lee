9eb6c63f345be3ca06c9d79a3e03c75df6450493
KimChanyoung <qurioturio@gmail.com>
2021-12-03 23:45:11 +0900

    6_CrystalCave까지 대략적인 설계

---
+++ Assets/Scripts/Puzzle/CellsKeybox.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellsKeybox : MonoBehaviour
+{
+    public GameObject ironDoor;
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     if(other.transform.name == "IronDoor")
+    //     {
+    //         CellsPuzzleStep.isClear = true;
+    //     }
+    // }
+}
